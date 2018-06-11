f5cbd7a45c45b667b359417f8fa0065fbddc3c16
bryansm22 <bsalvador1994@gmail.com>
2018-06-11 08:38:58 -0600

    Primera subida

---
+++ adivinapal.sh
@@ -0,0 +1,59 @@
+!/bin/bash
+EDITOR=nano
+PASSWD=/etc/passwd
+RED='\033[0;41;30m'
+STD='\033[0;0;39m'
+ 
+pause(){
+  read -p "Press [Enter] key to continue..." fackEnterKey
+}
+
+one(){
+	echo "one() called"
+        pause
+}
+ 
+# do something in two()
+two(){
+	echo "two() called"
+        pause
+}
+ 
+# function to display menus
+show_menus() {
+	clear
+	echo "-------------------"	
+	echo "     Ahorcado      "
+	echo "-------------------"
+	echo "1. Logearse"
+	echo "2. Registrarse"
+	echo "3. Salir"
+	echo "4. Manual de usuario"
+}
+
+read_options(){
+	local choice
+	read -p "Enter choice [ 1 - 3] " choice
+	case $choice in
+		1) one ;;
+		2) two ;;
+		3) exit 0;;
+		4) four ;;
+		*) echo -e "${RED}Error...${STD}" && sleep 2
+	esac
+}
+ 
+# ----------------------------------------------
+# Trap CTRL+C, CTRL+Z and quit singles
+# ----------------------------------------------
+trap '' SIGINT SIGQUIT SIGTSTP
+ 
+# -----------------------------------
+# Main logic - infinite loop
+# ------------------------------------
+while true
+do
+ 
+	show_menus
+	read_options
+done
